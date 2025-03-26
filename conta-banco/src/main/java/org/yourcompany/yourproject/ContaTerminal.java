2b3cf39faad116cc2458a14a580f6ccba52bc003
Heitor-Saqueto <heitorsaqueto@hotmail.com>
2025-03-26 00:42:00 -0300

    done

--- conta-banco/src/main/java/org/yourcompany/yourproject/ContaTerminal.java
+++ conta-banco/src/main/java/org/yourcompany/yourproject/ContaTerminal.java
@@ -4,13 +4,22 @@
 
 package org.yourcompany.yourproject;
 
+import java.util.Scanner;
+
 /**
  *
- * @author Heitor
+ * @author Heitor Saqueto
  */
 public class ContaTerminal{
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       
+     Scanner sc = new Scanner(System.in);
+     Integer numero = sc.nextInt();
+     String agencia = sc.next();
+     String nomeCliente = sc.next();
+     Double saldo = sc.nextDouble();
+
+     System.out.println("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+numero+" e seu saldo "+saldo+" já está disponível para saque");
     } 
 }
